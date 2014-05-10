b209a61f5f2ccba50eb24e90c93a7ef6d79345a1
Damian Piątkowski <damian.dem@gmail.com>
2014-05-10 13:41:08 +0200

    experimental Nullable monadic extensions

---
+++ OptionType.Tests/LinqToNullablesTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace whiteshore.OptionType.Tests
+{
+    public class LinqToNullablesTests
+    {
+        [Fact]
+        public void AddingTwoPresentNumberWorks()
+        {
+            int? x = 5;
+            int? y = 3;
+
+            var value = from a in x
+                        from b in y
+                        select a + b;
+
+            Assert.Equal(value.Value, 8);
+        }
+
+        [Fact]
+        public void AddingTwoNumberWhereOneIsAbsentYieldsNone()
+        {
+            int? x = 2;
+            int? y = null;
+
+            var value = from a in x
+                        from b in y
+                        select a + b;
+
+            Assert.False(value.HasValue);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-OptionType
-==========
+# OptionType
 
-Option monad in C#
+Experimental and boring implementation of Option monad in C#.
+
+## Rationale
+
+We all don't like null references crawling in our code and causing NullReferenceExceptions. Here's the solution.
+
